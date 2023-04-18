0a1529bb7751504fbdce32f8686ee6ce15e18fa1
Roland <rolandferrao@yahoo.com>
2023-04-18 11:11:47 -0400

    update files

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,13 @@
-from google.cloud import aiplatform as aip
+# from google.cloud import aiplatform as aip
 
-from kfp.v2 import dsl
-from kfp.v2 import compiler
-from kfp.v2.dsl import component
+from kfp import dsl
+from kfp import compiler
+# from kfp.v2.dsl import component
 
-from kfp.v2 import compiler  # noqa: F811
-from google_cloud_pipeline_components.v1.endpoint import (EndpointCreateOp,
-                                                              ModelDeployOp)
-from google_cloud_pipeline_components.v1.model import ModelUploadOp
+from kfp import compiler  # noqa: F811
+# from google_cloud_pipeline_components.v1.endpoint import (EndpointCreateOp,
+#                                                               ModelDeployOp)
+# from google_cloud_pipeline_components.v1.model import ModelUploadOp
 
 from pipeline_components import (
     perform_eda,
@@ -18,27 +18,26 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-PROJECT_ID = os.getenv("PROJECT_ID")
-REGION = os.getenv("REGION")
+# PROJECT_ID = os.getenv("PROJECT_ID")
+# REGION = os.getenv("REGION")
 DEST_BUCKET_URI = os.getenv("DEST_BUCKET_URI")
 SOURCE_FILE = os.getenv("SOURCE_FILE")
-PIPELINE_BUCKET_URI = os.getenv("PIPELINE_BUCKET_URI")
-SERVICE_ACCOUNT = os.getenv("SERVICE_ACCOUNT")
+# PIPELINE_BUCKET_URI = os.getenv("PIPELINE_BUCKET_URI")
+# SERVICE_ACCOUNT = os.getenv("SERVICE_ACCOUNT")
 
 # API service endpoint
-API_ENDPOINT = "{}-aiplatform.googleapis.com".format(REGION)
-PIPELINE_ROOT = "{}/pipeline_root/intro".format(PIPELINE_BUCKET_URI)
+# API_ENDPOINT = "{}-aiplatform.googleapis.com".format(REGION)
+# PIPELINE_ROOT = "{}/pipeline_root/intro".format(PIPELINE_BUCKET_URI)
 # print(f"PIPELINE_ROOT DEBUG: {PIPELINE_ROOT}")
 
 @dsl.pipeline(
     name="churn-prediction-pipeline",
-    description="Getting churn predictions",
-    pipeline_root=PIPELINE_ROOT,
+    description="Getting churn predictions"
 )
 def pipeline(dest_bucket_uri: str, source_file: str):
 
     # TODO: get just the run id number from this string
-    run_id = dsl.PIPELINE_JOB_NAME_PLACEHOLDER.split("-")[-1]
+    run_id = dsl.RUN_ID_PLACEHOLDER
     
     # input_data_task = (
     #     ingest_data(source_bucket_uri = source_bucket_uri).
@@ -87,28 +86,9 @@ def pipeline(dest_bucket_uri: str, source_file: str):
 if __name__=="__main__":
 
 
-    aip.init(project=PROJECT_ID, staging_bucket=PIPELINE_BUCKET_URI)
+    # aip.init(project=PROJECT_ID, staging_bucket=PIPELINE_BUCKET_URI)
 
-    compiler.Compiler().compile(pipeline_func=pipeline, package_path="churn_pipeline.json")
-
-    DISPLAY_NAME = "churn_pipeline"
-
-    job = aip.PipelineJob(
-        display_name=DISPLAY_NAME,
-        template_path="churn_pipeline.json",
-        # pipeline_root is where information is saved off for every run
-        pipeline_root=PIPELINE_ROOT,
-        enable_caching=False,
-        parameter_values = {
-            "dest_bucket_uri": DEST_BUCKET_URI,
-            "source_file": SOURCE_FILE
-        }
-    )
-
-    print(type(job))
-
-    job.run()
-
+    # compiler.Compiler().compile(pipeline_func=pipeline, package_path="churn_pipeline.json")
+    compiler.Compiler().compile(pipeline, 'pipeline.tar.gz', type_check=True)
     # clean up
     # training triger input 
-    # deploy - scheduled (?) trigger POSTMAN
