51522956e9baebe162f872aa8b09085f025e42b1
Roland <rolandferrao@yahoo.com>
2023-04-21 14:35:43 -0400

    update pipeline

--- data/pipeline.py
+++ data/pipeline.py
@@ -2,10 +2,12 @@
 
 from kfp import dsl
 from kfp import compiler
-from kfp.components import load_component_from_url
+# from kfp.v2.dsl import component
+
+from kfp import compiler  # noqa: F811
 # from google_cloud_pipeline_components.v1.endpoint import (EndpointCreateOp,
 #                                                               ModelDeployOp)
-from google_cloud_pipeline_components.v1.model import ModelUploadOp
+# from google_cloud_pipeline_components.v1.model import ModelUploadOp
 
 from pipeline_components import (
     perform_eda,
@@ -16,10 +18,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-kserve = load_component_from_url("https://raw.githubusercontent.com/kubeflow/pipelines/56edebb646ca471409a24addc40ab1c040f9fa1a/components/kserve/component.yaml")
-
-PROJECT_ID = os.getenv("PROJECT_ID")
+# PROJECT_ID = os.getenv("PROJECT_ID")
 # REGION = os.getenv("REGION")
 DEST_BUCKET_URI = os.getenv("DEST_BUCKET_URI")
 SOURCE_FILE = os.getenv("SOURCE_FILE")
@@ -35,10 +34,10 @@ SOURCE_FILE = os.getenv("SOURCE_FILE")
     name="churn-prediction-pipeline",
     description="Getting churn predictions"
 )
-def pipeline(dest_bucket_uri: str, source_file: str, ):
+def pipeline(dest_bucket_uri: str, source_file: str):
 
     # TODO: get just the run id number from this string
-    # run_id = dsl.PIPELINE_JOB_NAME_PLACEHOLDER.split("-")[-1]
+    run_id = dsl.RUN_ID_PLACEHOLDER
     
     # input_data_task = (
     #     ingest_data(source_bucket_uri = source_bucket_uri).
@@ -47,10 +46,8 @@ def pipeline(dest_bucket_uri: str, source_file: str, ):
     #     set_display_name("Ingest Data")
     # )
 
-    model_uri=f"gcs://{dest_bucket_uri}/model_file"
-    
     perform_eda_task = (
-        perform_eda(run_id = "run_id_1", 
+        perform_eda(run_id = run_id, 
                     dest_bucket_uri = dest_bucket_uri,
                     source_file = source_file).\
             set_cpu_limit('1').\
@@ -58,45 +55,19 @@ def pipeline(dest_bucket_uri: str, source_file: str, ):
             set_display_name("Ingest Data & Perform EDA")#.\
     )
     
-    train_task = train(run_id = "run_id_2", 
+    train_task = train(run_id = run_id, 
           dest_bucket_uri = dest_bucket_uri, 
           source_file = source_file).\
         set_display_name("Train Models").\
         after(perform_eda_task)
     
-    model_upload_op = ModelUploadOp(
-        project=PROJECT_ID,
-        display_name='model_upload',
-        unmanaged_container_model=dest_bucket_uri,
-    )
-    model_upload_op.after(train_task)
+    # model_upload_op = ModelUploadOp(
+    #     project=PROJECT_ID,
+    #     display_name='model_upload',
+    #     unmanaged_container_model=dest_bucket_uri,
+    # )
+    # model_upload_op.after(train_task)
 
-    model_uri = str(model_uri)
-    # pylint: disable=unused-variable
-    isvc_yaml = """
-    apiVersion: "serving.kserve.io/v1beta1"
-    kind: "InferenceService"
-    metadata:
-      name: "model-deploy"
-      namespace: "kubeflow-user-example-com"
-    spec:
-      predictor:
-        serviceAccountName: sa
-        pytorch:
-          protocolVersion: v2
-          storageUri: {}
-          resources:
-            requests: 
-              cpu: 4
-              memory: 8Gi
-            limits:
-              cpu: 4
-              memory: 8Gi
-    """.format(model_uri)
-
-    deploy_task = kserve(action="apply", inferenceservice_yaml=isvc_yaml
-                 ).after(model_upload_op).set_display_name("Deployer")
-    
     # endpoint_create_op = EndpointCreateOp(
     #     project=PROJECT_ID,
     #     display_name="pipelines-created-endpoint",
@@ -119,7 +90,5 @@ if __name__=="__main__":
 
     # compiler.Compiler().compile(pipeline_func=pipeline, package_path="churn_pipeline.json")
     compiler.Compiler().compile(pipeline, 'pipeline.tar.gz', type_check=True)
-
     # clean up
     # training triger input 
-    # deploy - scheduled (?) trigger POSTMAN
